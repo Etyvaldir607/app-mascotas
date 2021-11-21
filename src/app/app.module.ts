1f964b894f8d928fb71789093a7152e23512c476
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-11-21 04:00:56 -0400

    dwclaracion e importacion de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FelinosComponent } from './components/felinos/felinos.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { StoreComponent } from './components/store/store.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FelinosComponent,
+    GaleryComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
