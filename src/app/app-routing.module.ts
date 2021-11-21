b0ca7288841b565edeaad8b3bd6ac3b27154b4ba
Erick Valdir Machicado Calle <etyvaldir@gmail.com>
2021-11-21 04:08:24 -0400

    definición de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FelinosComponent } from './components/felinos/felinos.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { StoreComponent } from './components/store/store.component';
+
+
+
+const routes: Routes = [
+  { path: 'felinos-component', component: FelinosComponent },
+  { path: 'galery-component', component: GaleryComponent },
+  { path: 'store-component', component: StoreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {  }
